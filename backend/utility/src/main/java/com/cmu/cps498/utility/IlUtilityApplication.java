2c4e5327fca31b5a7f0d720d99644d936ccbe623
drewburton <drew.burton.gc@gmail.com>
2025-01-24 15:29:12 -0500

    created backend and utility folder and initialized utility project with dependencies. Commented out spring data jpa until datasource url is acquired. Created basic .gitignore for common files

---
+++ backend/utility/src/main/java/com/cmu/cps498/utility/IlUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.cmu.cps498.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlUtilityApplication.class, args);
+	}
+
+}
---
+++ backend/utility/src/test/java/com/cmu/cps498/utility/IlUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmu.cps498.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
