1d21874c846012390d3266c577e41d92874a941c
drewburton <drew.burton.gc@gmail.com>
2025-01-30 13:10:56 -0500

    tried uncapitalizing pages. Will this work? Probably not but I am still gonna try it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
 
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/HomePage';
-import TeacherDashboard from './Pages/TeacherDashboard';
+import HomePage from './pages/HomePage';
+import TeacherDashboard from './pages/TeacherDashboard';
 import Header from './Components_JS/Header';
 import Footer from './Components_JS/Footer';
 import './Styles/Components/App.scss'
 
 const App = () => {
-    return (
-        <div className="App">
-          <Header />
-          <Routes>
-            {/* Home Page Route */}
-            <Route path="/" element={<HomePage />} />
-            
-            {/* Teacher Dashboard Route */}
-            <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
-          </Routes>
-          <Footer />
-        </div>
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        {/* Home Page Route */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Teacher Dashboard Route */}
+        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
