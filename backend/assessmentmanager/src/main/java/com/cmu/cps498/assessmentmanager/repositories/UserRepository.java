c9ca5b616529808e7551dce5f66acd91255e8db0
drewburton <drew.burton.gc@gmail.com>
2025-02-07 15:57:35 -0500

    authenticated assessment manager to authenticate with token generated by utility service

---
+++ backend/assessmentmanager/src/main/java/com/cmu/cps498/assessmentmanager/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cmu.cps498.assessmentmanager.repositories;
+
+import com.cmu.cps498.assessmentmanager.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
