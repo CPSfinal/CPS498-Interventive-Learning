8434331b50dfcc800a5be0612255d4fa851c96db
Thomas Alfano <alfano1tj@alma.edu>
2025-02-10 11:52:03 -0500

    Add @CrossOrigin tag to /auth endpoints to allow requests from localhost

--- backend/utility/src/main/java/com/cmu/cps498/utility/controllers/AuthController.java
+++ backend/utility/src/main/java/com/cmu/cps498/utility/controllers/AuthController.java
@@ -8,11 +8,13 @@ import com.cmu.cps498.utility.services.AuthService;
 import com.cmu.cps498.utility.services.JwtService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
